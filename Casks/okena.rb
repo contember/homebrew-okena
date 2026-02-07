c9be338568fe55db76fab300b758f6c705bc26ae
jonasnobile <mesons_rates0p@icloud.com>
2026-02-07 21:16:44 +0100

    Initial tap with Okena cask v0.4.3

---
+++ Casks/okena.rb
@@ -0,0 +1,30 @@
+# Homebrew Cask for Okena
+# This file is automatically updated by CI on release.
+# To install: brew tap contember/okena && brew install --cask okena
+
+cask "okena" do
+  arch arm: "arm64", intel: "x64"
+
+  version "0.4.3"
+  sha256 arm:   "d36917f4f0b497e157be463577827f37dc6c640038c08bf5502828167da450f6",
+         intel: "0bd78f14b4833c5a4aeb277258c7b49784d5c0c6a8d1c59e03e574efd32240da"
+
+  url "https://github.com/contember/okena/releases/download/v#{version}/okena-macos-#{arch}.zip"
+  name "Okena"
+  desc "Terminal multiplexer for managing multiple terminal sessions"
+  homepage "https://github.com/contember/okena"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "Okena.app"
+
+  zap trash: [
+    "~/.config/okena",
+    "~/Library/Application Support/okena",
+    "~/Library/Caches/okena",
+    "~/Library/Preferences/com.contember.okena.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+# Homebrew Tap for Okena
+
+Install [Okena](https://github.com/contember/okena), a fast terminal multiplexer built in Rust.
+
+## Installation
+
+```bash
+brew tap contember/okena
+brew install --cask okena
+```
+
+## Updating
+
+```bash
+brew upgrade --cask okena
+```
